e51b63747715e11e491591d43edd8733bc01c2df
fguru06 <fhassan06@gmail.com>
2025-05-17 09:53:04 -0400

    Broke down files

---
+++ script.js
@@ -0,0 +1,65 @@
+function addMusaPoints() {
+    const musaPointsElement = document.getElementById('musaPoints');
+    let musaPoints = parseInt(musaPointsElement.textContent, 10);
+    musaPoints++;
+    musaPointsElement.textContent = musaPoints;
+}
+
+function subtractMusaPoints() {
+    const musaPointsElement = document.getElementById('musaPoints');
+    let musaPoints = parseInt(musaPointsElement.textContent, 10);
+    musaPoints--;
+    musaPointsElement.textContent = musaPoints;
+}
+
+function resetPoints() {
+    document.getElementById('musaPoints').textContent = '0';
+    document.getElementById('jozyPoints').textContent = '0';
+}
+
+function addJozyPoints() {
+    const jozyPointsElement = document.getElementById('jozyPoints');
+    let jozyPoints = parseInt(jozyPointsElement.textContent, 10);
+    jozyPoints++;
+    jozyPointsElement.textContent = jozyPoints;
+}
+
+function subtractJozyPoints() {
+    const jozyPointsElement = document.getElementById('jozyPoints');
+    let jozyPoints = parseInt(jozyPointsElement.textContent, 10);
+    jozyPoints--;
+    jozyPointsElement.textContent = jozyPoints;
+}
+
+function addMusaPointss() {
+    const musaPointsElement = document.getElementById('musaPointss');
+    let musaPoints = parseInt(musaPointsElement.textContent, 10);
+    musaPoints++;
+    musaPointsElement.textContent = musaPoints;
+}
+
+function subtractMusaPointss() {
+    const musaPointsElement = document.getElementById('musaPointss');
+    let musaPoints = parseInt(musaPointsElement.textContent, 10);
+    musaPoints--;
+    musaPointsElement.textContent = musaPoints;
+}
+
+function resetPointss() {
+    document.getElementById('musaPointss').textContent = '0';
+    document.getElementById('jozyPointss').textContent = '0';
+}
+
+function addJozyPointss() {
+    const jozyPointsElement = document.getElementById('jozyPointss');
+    let jozyPoints = parseInt(jozyPointsElement.textContent, 10);
+    jozyPoints++;
+    jozyPointsElement.textContent = jozyPoints;
+}
+
+function subtractJozyPointss() {
+    const jozyPointsElement = document.getElementById('jozyPointss');
+    let jozyPoints = parseInt(jozyPointsElement.textContent, 10);
+    jozyPoints--;
+    jozyPointsElement.textContent = jozyPoints;
+}
